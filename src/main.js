93cb4ff5069575c5adfe57f2e3782bc379896841
René Kooi <rene@kooi.me>
2015-08-12 00:28:55 +0200

    show user id and level in chat messages

--- src/main.js
+++ src/main.js
@@ -1,17 +1,55 @@
 define(function (require, exports, module) {
 
   const Plugin = require('extplug/Plugin');
+  const Events = require('plug/core/Events');
+  const $ = require('jquery');
 
   const UserInfo = Plugin.extend({
     name: 'Moderation: User Info',
     description: 'Displays user IDs and levels inline in chat and user rollovers.',
 
-    enable() {
-      // code to start your plugin
+    style: {
+      '.extplug-inline-id, .extplug-inline-level': {
+        'color': '#555d70'
+      },
+      '#chat-messages': {
+        '.extplug-inline-id': {
+          'padding-left': '5px'
+        },
+        '.extplug-inline-level::before': {
+          'content': '"|"',
+          'padding': '0 3px'
+        }
+      }
     },
 
-    disable() {
-      // code to undo what you did in enable()
+    enable() {
+      this.listenTo(Events, 'chat:afterreceive', this.onMessage);
+    },
+
+    onMessage(message, el) {
+      if (!message.uid) return;
+      let user = API.getUser(message.uid);
+      let un = el.find('.un');
+
+      if (el.hasClass('inline')) {
+        let text = user ? `ID: ${message.uid} | Level: ${user.level}` :
+          `ID: ${message.uid}`;
+        un.attr('data-tooltip', text);
+      }
+      else {
+        let uid = $('<span />')
+          .addClass('extplug-inline-id')
+          .text(`ID: ${message.uid}`)
+          .insertAfter(un);
+
+        if (user) {
+          $('<span />')
+            .addClass('extplug-inline-level')
+            .text(`Level: ${user.level}`)
+            .insertAfter(uid);
+        }
+      }
     }
   });
 
