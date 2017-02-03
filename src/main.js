e785df147b20af781a11d79a54b3d4aa02343184
René Kooi <renee@kooi.me>
2017-02-03 15:49:57 +0100

    ES Modulesify.

--- src/main.js
+++ src/main.js
@@ -1,62 +1,46 @@
-define(function (require, exports, module) {
+import Plugin from 'extplug/Plugin';
+import Events from 'plug/core/Events';
+import $ from 'jquery';
+import style from './style.css';
 
-  const Plugin = require('extplug/Plugin');
-  const Events = require('plug/core/Events');
-  const $ = require('jquery');
+const DATA_KEY = 'extplug-inline-user-info-added';
 
-  const DATA_KEY = 'extplug-inline-user-info-added';
+const UserInfo = Plugin.extend({
+  name: 'Inline User Info',
+  description: 'Displays user IDs and levels inline in chat and user rollovers.',
 
-  const UserInfo = Plugin.extend({
-    name: 'Inline User Info',
-    description: 'Displays user IDs and levels inline in chat and user rollovers.',
+  style,
 
-    style: {
-      '.extplug-inline-id, .extplug-inline-level': {
-        'color': '#555d70'
-      },
-      '#chat-messages': {
-        '.extplug-inline-id': {
-          'padding-left': '5px'
-        },
-        '.extplug-inline-level::before': {
-          'content': '"|"',
-          'padding': '0 3px'
-        }
+  enable() {
+    this.listenTo(Events, 'chat:afterreceive', this.onMessage);
+  },
+
+  onMessage(message, el) {
+    if (!message.uid || el.data(DATA_KEY)) return;
+    const user = API.getUser(message.uid);
+    const un = el.find('.un');
+
+    if (el.hasClass('inline')) {
+      const text = user
+        ? `ID: ${message.uid} | Level: ${user.level}`
+        : `ID: ${message.uid}`;
+      un.attr('data-tooltip', text);
+    } else {
+      const uid = $('<span />')
+        .addClass('extplug-inline-id')
+        .text(`ID: ${message.uid}`)
+        .insertAfter(un);
+
+      if (user) {
+        $('<span />')
+          .addClass('extplug-inline-level')
+          .text(`Level: ${user.level}`)
+          .insertAfter(uid);
       }
-    },
-
-    enable() {
-      this.listenTo(Events, 'chat:afterreceive', this.onMessage);
-    },
-
-    onMessage(message, el) {
-      if (!message.uid || el.data(DATA_KEY)) return;
-      let user = API.getUser(message.uid);
-      let un = el.find('.un');
-
-      if (el.hasClass('inline')) {
-        let text = user ? `ID: ${message.uid} | Level: ${user.level}` :
-          `ID: ${message.uid}`;
-        un.attr('data-tooltip', text);
-      }
-      else {
-        let uid = $('<span />')
-          .addClass('extplug-inline-id')
-          .text(`ID: ${message.uid}`)
-          .insertAfter(un);
-
-        if (user) {
-          $('<span />')
-            .addClass('extplug-inline-level')
-            .text(`Level: ${user.level}`)
-            .insertAfter(uid);
-        }
-      }
-
-      el.data(DATA_KEY, true);
     }
-  });
 
-  module.exports = UserInfo;
-
+    el.data(DATA_KEY, true);
+  }
 });
+
+export default UserInfo;
